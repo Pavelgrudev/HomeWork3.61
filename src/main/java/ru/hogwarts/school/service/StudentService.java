7347bf59d75ff78d5a5d5e0e812d2da8546b0f77
Павел <Pavel-grudev@yandex.ru>
2023-06-27 17:00:19 +0300

    Branch1 , check1

---
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -0,0 +1,21 @@
+package ru.hogwarts.school.service;
+
+import ru.hogwarts.school.model.Student;
+
+import java.util.Collection;
+
+public interface StudentService {
+    Student add(Student student);
+
+    Student remove(Long id);
+
+    Student update(Student student);
+
+    Collection<Student> getAll();
+
+    Student get(Long id);
+
+    Collection<Student> getByAge(Integer startAge, Integer endAge);
+
+}
+
